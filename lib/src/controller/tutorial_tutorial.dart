3e1ffbeca5c7bea91b2a5a3bb898edfd10aa2b64
DESKTOP-BOR4GSQ\Dereumaux-Adrien <dereumauxadrien@gmail.com>
2021-07-02 21:15:58 +0200

    Added empty globalKey possibility to allow for tutorial items without a focus

--- lib/src/controller/tutorial_tutorial.dart
+++ lib/src/controller/tutorial_tutorial.dart
@@ -12,73 +12,130 @@ class Tutorial {
     OverlayState overlayState = Overlay.of(context)!;
     List<OverlayEntry> entries = [];
     children.forEach((element) async {
-      var offset = _capturePositionWidget(element.globalKey!);
-      var sizeWidget = _getSizeWidget(element.globalKey!);
-      entries.add(
-        OverlayEntry(
-          builder: (context) {
-            return GestureDetector(
-              onTap: element.touchScreen == true
-                  ? () {
-                      entries[count].remove();
-                      count++;
-                      if (count != entries.length) {
-                        overlayState.insert(entries[count]);
+      if (element.globalKey != null) {
+        var offset = _capturePositionWidget(element.globalKey!);
+        var sizeWidget = _getSizeWidget(element.globalKey!);
+        entries.add(
+          OverlayEntry(
+            builder: (context) {
+              return GestureDetector(
+                onTap: element.touchScreen == true
+                    ? () {
+                        entries[count].remove();
+                        count++;
+                        if (count != entries.length) {
+                          overlayState.insert(entries[count]);
+                        }
                       }
-                    }
-                  : () {},
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Stack(
-                  children: [
-                    CustomPaint(
-                      size: size,
-                      painter: HolePainter(
-                          shapeFocus: element.shapeFocus,
-                          dx: offset.dx + (sizeWidget.width / 2),
-                          dy: offset.dy + (sizeWidget.height / 2),
-                          width: sizeWidget.width,
-                          height: sizeWidget.height,
-                          color: element.color,
-                          borderRadius: element.borderRadius),
-                    ),
-                    Positioned(
-                      top: element.top,
-                      bottom: element.bottom,
-                      left: element.left,
-                      right: element.right,
-                      child: Container(
-                        width: size.width * 0.8,
-                        child: Column(
-                          crossAxisAlignment: element.crossAxisAlignment,
-                          mainAxisAlignment: element.mainAxisAlignment,
-                          children: [
-                            ...element.children!,
-                            GestureDetector(
-                              child: element.widgetNext ??
-                                  Text(
-                                    "NEXT",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                              onTap: () {
-                                entries[count].remove();
-                                count++;
-                                if (count != entries.length) {
-                                  overlayState.insert(entries[count]);
-                                }
-                              },
-                            ),
-                          ],
-                        ),
+                    : () {},
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Stack(
+                    children: [
+                      CustomPaint(
+                        size: size,
+                        painter: HolePainter(
+                            shapeFocus: element.shapeFocus,
+                            dx: offset.dx + (sizeWidget.width / 2),
+                            dy: offset.dy + (sizeWidget.height / 2),
+                            width: sizeWidget.width,
+                            height: sizeWidget.height,
+                            color: element.color,
+                            borderRadius: element.borderRadius),
                       ),
-                    )
-                  ],
+                      Positioned(
+                        top: element.top,
+                        bottom: element.bottom,
+                        left: element.left,
+                        right: element.right,
+                        child: Container(
+                          width: size.width * 0.8,
+                          child: Column(
+                            crossAxisAlignment: element.crossAxisAlignment,
+                            mainAxisAlignment: element.mainAxisAlignment,
+                            children: [
+                              ...element.children!,
+                              GestureDetector(
+                                child: element.widgetNext ??
+                                    Text(
+                                      "NEXT",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                onTap: () {
+                                  entries[count].remove();
+                                  count++;
+                                  if (count != entries.length) {
+                                    overlayState.insert(entries[count]);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
-      );
+              );
+            },
+          ),
+        );
+      } else{
+        entries.add(
+          OverlayEntry(
+            builder: (context) {
+              return GestureDetector(
+                onTap: element.touchScreen == true
+                    ? () {
+                  entries[count].remove();
+                  count++;
+                  if (count != entries.length) {
+                    overlayState.insert(entries[count]);
+                  }
+                }
+                    : () {},
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Stack(
+                    children: [
+                      Positioned(
+                        top: element.top,
+                        bottom: element.bottom,
+                        left: element.left,
+                        right: element.right,
+                        child: Container(
+                          width: size.width * 0.8,
+                          child: Column(
+                            crossAxisAlignment: element.crossAxisAlignment,
+                            mainAxisAlignment: element.mainAxisAlignment,
+                            children: [
+                              ...element.children!,
+                              GestureDetector(
+                                child: element.widgetNext ??
+                                    Text(
+                                      "NEXT",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                onTap: () {
+                                  entries[count].remove();
+                                  count++;
+                                  if (count != entries.length) {
+                                    overlayState.insert(entries[count]);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      }
     });
 
     overlayState.insert(entries[0]);
