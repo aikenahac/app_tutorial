14a3c5a36b485d5e7b09387f0e5191205aa3e5c6
asharulislam <immaster35@gmail.com>
2023-07-30 20:56:07 +0500

    added a onTutorialComplete callback which will called after the tutorial ends.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,10 @@
 ## [0.1.0] - Create fork from tutorial package
 
-* First commit
+- First commit
 
 ## [0.2.0] - New features
 
-* Add support for custom background colors and new shape
+- Add support for custom background colors and new shape
 
 ## [0.2.1] - Update README.md
 
@@ -19,9 +19,11 @@
 ## [0.3.2] - Format project
 
 ## [1.0.0] - First release
+
 Includes breaking changes: TutorialItems -> TutorialItem
 
 ## [1.0.1] - Updated base model
+
 Major change on the TutorialItem model. See the documentation
 
 ## [1.0.2] - Updated example
@@ -29,6 +31,7 @@ Major change on the TutorialItem model. See the documentation
 ## [1.0.3] - Updated default values
 
 ## [1.0.4] - Allow for more control
+
 This update permits users to have more control over the onboarding flow
 
 - Use the `Tutorial.skipAll()` method to skip the tutorial
@@ -36,3 +39,5 @@ This update permits users to have more control over the onboarding flow
 - Added the `radius` property, which will be used to set a custom radius for the `ShapeFocus.oval` shape
 
 ## [1.0.5] - Updated docs
+
+## [1.0.6] - added a onTutorialComplete callback which will called after the tutorial ends.
--- lib/src/controller/tutorial_tutorial.dart
+++ lib/src/controller/tutorial_tutorial.dart
@@ -1,21 +1,26 @@
 library tutorial;
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:app_tutorial/src/models/tutorial_item.dart';
 import 'package:app_tutorial/src/painter/painter.dart';
 
-/// This is the main class of the app.
 class Tutorial {
   static List<OverlayEntry> entries = [];
   static late int count;
 
-  /// The method that shows the tutorial
-  static showTutorial(BuildContext context, List<TutorialItem> children) async {
+  static Future<void> showTutorial(
+      BuildContext context, List<TutorialItem> children,
+      {required VoidCallback onTutorialComplete}) async {
     clearEntries();
     final size = MediaQuery.of(context).size;
     OverlayState overlayState = Overlay.of(context);
 
     count = 0;
+
+    // Create a Completer to indicate when the tutorial is complete
+    Completer<void> tutorialCompleter = Completer<void>();
 
     children.forEach((element) async {
       final offset = _capturePositionWidget(element.globalKey);
@@ -29,6 +34,9 @@ class Tutorial {
                 count++;
                 if (count < entries.length) {
                   overlayState.insert(entries[count]);
+                } else {
+                  // If this is the last tutorial step, complete the tutorial
+                  tutorialCompleter.complete();
                 }
               },
               child: Scaffold(
@@ -59,6 +67,12 @@ class Tutorial {
     });
 
     overlayState.insert(entries[0]);
+
+    // Wait until the tutorialCompleter.future is completed to indicate the tutorial is finished
+    await tutorialCompleter.future;
+
+    // If the onTutorialComplete function is provided, call it
+    onTutorialComplete();
   }
 
   static clearEntries() {
